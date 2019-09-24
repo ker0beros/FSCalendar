43d4deccaee6da76cd168762ba1a2cf84fc688f2
Khairul Azmi <azmi@getslurp.com>
2019-09-24 09:11:20 +0800

    fix no comma

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "FSCalendar",
 	platforms: [
-		.iOS(.v12),
-	]
+		.iOS(.v12)
+	],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
